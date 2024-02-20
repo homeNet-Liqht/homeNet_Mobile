accf766fa03b7127972790d93a0b2008e2fe9b14
wuaan <dangminhwuan@gmail.com>
2024-02-20 15:22:13 +0700

    feat/onboarding

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import AppNavigation from './src/navigation/appNavigation.tsx';
+import LinearGradient from 'react-native-linear-gradient';
 
 function App(): React.JSX.Element {
-  return <AppNavigation />;
+    return (
+
+        <AppNavigation/>
+
+    )
 }
 
 export default App;
--- src/components/onboarding.tsx
+++ src/components/onboarding.tsx
@@ -1,50 +1,90 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Dimensions, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
+import LinearGradient from "react-native-linear-gradient";
+import {useNavigation} from "@react-navigation/native";
 
+
+const {width, height} = Dimensions.get("window")
 const OnboardingScreen = () => {
-  // @ts-ignore
-  return (
-    <View style={styles.container}>
-      <Onboarding
-        containerStyles={{paddingHorizontal: 15}}
-        pages={[
-          {
-            backgroundColor: 'transparent',
+    const navigation = useNavigation();
+    const handleDone = () => {
+        navigation.navigate();
+    }
 
-            image: (
-              <View>
-                <Image source={require('../assets/oboarding1.png')} />
-              </View>
-            ),
-            title: '',
-            subtitle: 'Protect what matters most.',
-          },
-          {
-            backgroundColor: '#fff',
-            image: (
-              <View>
-                <Text>hihi</Text>
-              </View>
-            ),
-            title: '',
-            subtitle: 'Enhance your understanding of family members.',
-          },
-        ]}
-      />
-    </View>
-  );
+    const doneButton = ({...props}) =>{
+        return (
+            <TouchableOpacity style={styles.doneButton}>
+                <Text style={{color: "white", fontWeight: "bold"}}>Done</Text>
+            </TouchableOpacity>
+        )
+    }
+
+    // @ts-ignore
+    return (
+        <View style={styles.container}>
+            <Onboarding
+                onDone={handleDone}
+                onSkip={handleDone}
+                DoneButtonComponent={doneButton}
+                bottomBarHighlight={false}
+                containerStyles={{paddingHorizontal: 35}}
+                pages={[
+                    {
+                        backgroundColor: '#01B7FF',
+                        image: (
+                            <View >
+                                <Image source={require('../assets/oboarding1.png')}/>
+                            </View>
+                        ),
+                        title: 'Protect what matters most.',
+                        subtitle: '',
+                    },
+                    {
+                        backgroundColor: '#0BE5DC',
+                        image: (
+                            <View >
+                                <Image source={require('../assets/onboarding2.png')}/>
+                            </View>
+                        ),
+                        title: 'Enhance your understanding of family members.',
+                        subtitle: '',
+                    },
+                    {
+                        backgroundColor: '#0BE5DC',
+                        image: (
+                            <View >
+                                <Image style={styles.image} source={require('../assets/onboarding3.png')}/>
+                            </View>
+                        ),
+                        title: 'Enhance your understanding of family members.',
+                        subtitle: '',
+                    },
+                ]}
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  gradientContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+    },
+    gradientContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    image: {
+        objectFit: "fill",
+        width: 300,
+        height: 400
+    },
+    doneButton:{
+        padding: 20,
+        color: "white"
+    }
 });
 
 export default OnboardingScreen;
