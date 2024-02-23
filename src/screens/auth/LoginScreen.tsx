0adc4f580236245515dd366184970caeac4f1c49
wuaan <dangminhwuan@gmail.com>
2024-02-23 13:14:38 +0700

    add input component

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { View, Text} from "react-native";
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from "react-native";
 import {ButtonComponent} from "../../components/ButtonComponent.tsx";
+import InputComponent from "../../components/InputComponent.tsx";
+import {appColors} from "../../constants/appColors.ts";
+import {globalStyles} from "../styles/globalStyles.ts";
+import {Lock, Sms} from "iconsax-react-native";
 
 
 function LoginScreen() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     return (
-       <View>
-           <Text>LoginScreen</Text>
-           <ButtonComponent text={"login"} type={"primary"} />
-       </View>
+        <View style={[globalStyles.container,{padding: 20}]}>
+            <Text>LoginScreen</Text>
+            <InputComponent
+                value={email}
+                placeHolder={"Email"}
+                onChange={email => setEmail(email)}
+                isPassword={false}
+                allowClear
+                affix={<Sms size={22} color={appColors.gray}/> }/>
+            <InputComponent
+                value={password}
+                placeHolder={"Password"}
+                onChange={val => setPassword(val)}
+                isPassword
+                affix={<Lock size={22} color={appColors.gray}/> }/>
+            <ButtonComponent text={"login"} type={"primary"}/>
+        </View>
     );
 }
+
 
 export default LoginScreen;
