4d53fd4ef4f37fcbc77c4a48f5d459ca3f267b38
wuaan <dangminhwuan@gmail.com>
2024-03-19 11:17:35 +0700

    edit: edit profile

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -5,9 +5,9 @@ const userApi = {
     const url = `/user/current-user/`
     return axiosClient.get(url)
   },
-  editUser: (id: string, name: string, birthday: Date | null) => {
+  editUser: (id: string, name: string, birthday: Date | null, phone: string) => {
     const url = `/user/update-info/${id}`;
-    return axiosClient.put(url, { name: name, birthday: birthday })
+    return axiosClient.put(url, { name: name, birthday: birthday , phone: phone})
   },
     updateFCMToken: (uid: string, token: string[]) => {
         const url = `/user/update-fcmtoken/${uid}`;
